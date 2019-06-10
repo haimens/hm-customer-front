5937381ce2bfd9af555a4c1198a0a8e5c13c7b15
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-10 15:59:02 -0700

    css update

--- src/container/dashboard/Dashboard.container.js
+++ src/container/dashboard/Dashboard.container.js
@@ -7,7 +7,7 @@ import { DatePicker, TimePicker } from "antd";
 import GAutoComplete from "../../components/shared/GAutoComplete";
 import { saveDate, saveTime, savePassenger } from "../../actions/location.action";
 import MainCard from "../../components/shared/MainCard";
-import Footer from '../../components/nav/Footer.component';
+import Footer from "../../components/nav/Footer.component";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -40,12 +40,12 @@ class Dashboard extends Component {
           <Nav outerClass={"main-nav"} />
           <div className="container-fluid main-info">
             <div className="row">
-              <div className="col col-6">
+              <div className="col-md-6 d-none d-lg-block">
                 <h1 className="text-white header-text">Pickup & Go</h1>
               </div>
 
-              <div className="col col-6 ">
-                <div className="col-9 dashboard-form rounded p-4">
+              <div className="col-12 col-lg-6 ">
+                <div className="col-lg-9 col-12 dashboard-form rounded p-4">
                   <h3 className="haimens-main-textColor font-weight-bold ">BOOK A TRIP NOW</h3>
                   <div className="mt-4">
                     <GAutoComplete placeholder={"PICKUP"} />
@@ -54,9 +54,9 @@ class Dashboard extends Component {
                     <GAutoComplete placeholder={"DROPOFF"} />
                   </div>
 
-                  <div className="mt-3 d-flex">
+                  <div className="mt-3">
                     <div className="row">
-                      <div className="d-flex col-6">
+                      <div className="d-flex col-6 ">
                         <span className="input-group-text bg-white border-right-0 main-addon">
                           <i className="far fa-calendar-times addon-color" />
                         </span>
@@ -104,30 +104,54 @@ class Dashboard extends Component {
             <div className="container">
               <div className="row">
                 <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"TO AIRPORT: ON TIME GUARANTEED PICKUP"} 
-                  paragraph={"We Guarantee the driver will come to pick you up within the specified time frame for your airport drop off."} />
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"TO AIRPORT: ON TIME GUARANTEED PICKUP"}
+                    paragraph={
+                      "We Guarantee the driver will come to pick you up within the specified time frame for your airport drop off."
+                    }
+                  />
                 </div>
                 <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"FROM AIRPORT: LESS WAIT TIME AT THE CURB"} 
-                  paragraph={"90% of our customers wait 10 minutes or less at the curb."} />
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"FROM AIRPORT: LESS WAIT TIME AT THE CURB"}
+                    paragraph={"90% of our customers wait 10 minutes or less at the curb."}
+                  />
                 </div>
                 <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"PHONE SUPPORT"} 
-                  paragraph={"Feel free to call us for any question you may have before, on or after the trip."} />
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"PHONE SUPPORT"}
+                    paragraph={"Feel free to call us for any question you may have before, on or after the trip."}
+                  />
                 </div>
               </div>
               <div className="row card-margin-fix-sub">
-              <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"HOURLY CHARTER"} 
-                  paragraph={"Our Customer directed hourly charter service puts a chauffeur at your disposal for as long as you desire."} />
+                <div className="col-4 px-5">
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"HOURLY CHARTER"}
+                    paragraph={
+                      "Our Customer directed hourly charter service puts a chauffeur at your disposal for as long as you desire."
+                    }
+                  />
                 </div>
                 <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"QUALITY"} 
-                  paragraph={"Clean, comfortable, and safe vehicles that meet your travel needs."} />
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"QUALITY"}
+                    paragraph={"Clean, comfortable, and safe vehicles that meet your travel needs."}
+                  />
                 </div>
                 <div className="col-4 px-5">
-                  <MainCard img={"haimensMain.png"} title={"PROFESSIONAL DRIVERS"} 
-                  paragraph={"Experienced, Friendly, and service oriented drivers are ready to serve you in higher standard."} />
+                  <MainCard
+                    img={"haimensMain.png"}
+                    title={"PROFESSIONAL DRIVERS"}
+                    paragraph={
+                      "Experienced, Friendly, and service oriented drivers are ready to serve you in higher standard."
+                    }
+                  />
                 </div>
               </div>
             </div>
