78be65cb054561e8de4cc10d59574e51f89093cf
Jeremy Guan <jeremyguan1@gmail.com>
2019-06-10 15:34:20 -0700

    Update Footer.component.js

--- src/components/nav/Footer.component.js
+++ src/components/nav/Footer.component.js
@@ -4,7 +4,7 @@ import './Footer.component.css';
 class Footer extends Component {
   render() {
     return (
-      <footer className="page-footer font-small footer-index fixed-bottom shadow haimens-main-bgColor pt-4">
+      <footer className="page-footer font-small footer-index shadow haimens-main-bgColor py-4">
         <div className="footer-copyright text-center text-white footer-text ">
           Copyright 2019, Sunshire Shuttle Inc. All rights reserved.
         </div>
