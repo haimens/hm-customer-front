4f465e02d9ede656e70284da43a5ec3d3f46fb6a
Jeremy <jeremyguan1@gmail.com>
2019-07-28 14:19:15 -0700

    Update Nav.component.js

--- src/components/nav/Nav.component.js
+++ src/components/nav/Nav.component.js
@@ -99,7 +99,7 @@ class Nav extends Component {
                         className="dropdown-item px-0 d-flex justify-content-center align-items-center"
                         type="button"
                         onClick={() => {
-                          this.props.history.push("order/list");
+                          this.props.history.push("/order/list");
                         }}
                       >
                         <small>
