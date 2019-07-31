335afd3d0cb335095ef34946804168098c76a54b
Jeremy Guan <jeremyguan1@gmail.com>
2019-07-30 17:29:43 -0700

    Update localStorage.action.js

--- src/actions/localStorage.action.js
+++ src/actions/localStorage.action.js
@@ -10,7 +10,17 @@ export const loadUserInfo = type => {
 
 export const saveUserInfo = user_info => {
   try {
-    const { instance_token, user_token, username, customer_token, email, name, cell, img_path } = user_info;
+    const {
+      instance_token,
+      user_token,
+      username,
+      customer_token,
+      email,
+      name,
+      cell,
+      img_path,
+      realm_token
+    } = user_info;
     localStorage.setItem("instance_token", instance_token);
     localStorage.setItem("user_token", user_token);
     localStorage.setItem("username", username);
@@ -19,6 +29,7 @@ export const saveUserInfo = user_info => {
     localStorage.setItem("name", name);
     localStorage.setItem("cell", cell);
     localStorage.setItem("img_path", img_path);
+    localStorage.setItem("realm_token", realm_token);
   } catch (e) {
     console.log("save auth token error");
     throw e;
